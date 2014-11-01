82a425dc9ca34c9893060c5f9d01970d4731c0bc
mrsrubka <mr.SRUBKA@mail.ru>
2014-11-01 08:12:39 +0100

    usunięcie importu numpy, cv2, random ze script.py, ponieważ są potrzebne tylko dla klasy WatermarkImage

--- python/1/script.py
+++ python/1/script.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # import sys
 #sys.path.append('/usr/local/lib/python2.7/site-packages')
-import numpy as np
-import cv2
-import random
 from WatermarkImage import WatermarkImage
 
 img = WatermarkImage('lena.bmp')
