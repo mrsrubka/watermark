afa040a03daf6639ebff5963aa639091e9a8724b
mrsrubka <mr.SRUBKA@mail.ru>
2014-11-07 21:50:26 +0100

    cofnięcie usunięcia script.py

---
+++ python/1/script.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# import sys
+#sys.path.append('/usr/local/lib/python2.7/site-packages')
+from WatermarkImage import WatermarkImage
+#file_path = raw_input("Please enter file path")
+#users_message = raw_input("Please enter your message")
+file_path = 'lena.bmp'
+users_message = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+img = WatermarkImage(file_path, users_message)
+img.write_watermark()
+#####################################################################
+#file_with_message_path = raw_input("Please enter file path")
+#img_with_message_path = 'img_with_message.jpeg'
+#img_to_decode = WatermarkImage()
+#img_to_decode.set_img_with_message(img_with_message_path)
+#img_to_decode.read_watermark()
+img.read_watermark()
+#####################################################################
+img.write_all_images_to_files()
+print img.message_detected
